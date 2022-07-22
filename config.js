543d044a86554a3656ac6219ed3140d74f24d246
dltmddus1998 <dltmddus1998@naver.com>
2022-07-22 10:31:02 +0900

    Setting: 환경 초기 설정

--- config.js
+++ config.js
@@ -10,6 +10,13 @@ function required(key, defaultValue = undefined) {
 }
 
 export const config = {
+    jwt: {
+        secretKey: required('JWT_SECRET'),
+        expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 86400)),
+    },
+    bcrypt: {
+        saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', 12)),
+    },
     host: {
         port: 3000,
     },
